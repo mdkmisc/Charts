696652628b5223e4b4bf307d83cf42b2869e048e
Erik Campobadal <ConsoleTVs@gmail.com>
2016-09-26 14:36:41 +0200

    new default setting

--- config/charts.php
+++ config/charts.php
@@ -7,6 +7,7 @@ return [
     |--------------------------------------------------------------------------
     */
     'default'   => [
+        'type'          => 'line',
         'library'       => 'google',
         'element_label' => 'Element',
         'title'         => 'My chart',
